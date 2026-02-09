1c6da500a1b6457d3fb68bd176ea971fb42587d4
camicamila93 <camilaorso.sp@gmail.com>
2026-02-09 12:48:11 -0300

    .

--- js/api.js
+++ js/api.js
@@ -36,3 +36,22 @@ function actualizarHospedaje(id, data) {
 function eliminarHospedaje(id) {
   return API.delete(`/hospedajes/${id}`)
 }
+const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+const urlencoded = new URLSearchParams();
+urlencoded.append("id_cliente", "3000");
+urlencoded.append("celular", "5493415027238");
+urlencoded.append("password", "Carlos3135++");
+
+const requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: urlencoded,
+  redirect: "follow"
+};
+
+fetch("149.50.128.94:3010/login", requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
